6450f26278f4a517082bde9a02e7baebf50ef664
mgraziano <matigraziano14@gmail.com>
2023-02-21 10:32:48 -0300

    nuevo commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Bienvenidos a la calculadora de Promedios");
+/* alert("Bienvenidos a la calculadora de Promedios");
 const usuarioAutorizado = "matias";
 const contraseniaAutorizada = "1234";
 
@@ -42,3 +42,72 @@ function apruebaAnio(promedio) {
         return false;
     }
 }
+ */
+
+/* const baseDeDatos = [
+
+{
+    id: 1,
+    nombre: "Renault",
+    modelo: "2018",
+    precio: "30000",
+    color: "Blanco",
+    imagen: "/img/renault.jpg"
+},
+{
+    id: 2,
+    nombre: "Citroen",
+    modelo: "2017",
+    precio: "15000",
+    color: "Bordo",
+    imagen: "/img/citroen.jpg",
+},
+{
+    id: 3,
+    nombre: "Ford",
+    modelo: "2019",
+    precio: "20000",
+    color: "Blanco",
+    imagen: "/img/ford.jpg",
+},
+{
+    id: 4,
+    nombre: "Volkswagen",
+    modelo: "2018",
+    precio: "10000",
+    color: "Azul",
+    imagen: "/img/volkswagen.jpg",
+},
+]
+
+function pasarLista(auto, precio) {
+    console.log(`El auto marca ${auto.nombre} cuesta $${auto.precio} pesos`);
+}
+
+baseDeDatos.forEach((nombre, precio) => pasarLista(nombre, precio)); */
+
+/* const alumnosCoder = [
+
+{
+    nombre: "Matías",
+    edad: 25,
+},
+{
+    nombre: "Chiara",
+    edad: 26,
+},
+{
+    nombre: "Lola",
+    edad: 19
+},
+{
+    nombre: "Franco",
+    edad: 28,
+}
+]
+
+function pasarLista(estudiante, indice) {
+    console.log(`El estudiante número ${indice + 1} ${estudiante.nombre} tiene ${estudiante.edad} años`);
+}
+
+alumnosCoder.forEach((nombre, indice) => pasarLista(nombre, indice)); */
